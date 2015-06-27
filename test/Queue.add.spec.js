0c3a301d95560071516347db53791ad4a4a6471c
Jan Potoms <potoms.jan@gmail.com>
2015-06-27 17:30:59 +0200

    Experiment with ES2016 async/await tests

--- test/Queue.add.spec.js
+++ test/Queue.add.spec.js
@@ -13,128 +13,68 @@ describe('Queue.add', function () {
     return client.flushall();
   });
 
-  it('should add jobs', function () {
-    var idJob1;
-    return queue.add('job-data-1')
-      .then(function (id) {
-        return queue.get(id);
-      })
-      .then(function (job1) {
-        assert.isDefined(job1.id);
-        assert.strictEqual(job1.state, 'inactive');
-        assert.typeOf(job1.created, 'number');
-        idJob1 = job1.id;
-        assert.strictEqual(job1.data, 'job-data-1');
-        return queue.stat();
-      })
-      .then(function (stats) {
-        assert.propertyVal(stats, 'inactiveCount', 1);
-        return queue.add('job-data-2');
-      })
-      .then(function (id) {
-        return queue.get(id);
-      })
-      .then(function (job2) {
-        assert.isDefined(job2.id);
-        assert.notEqual(idJob1, job2.id);
-        assert.strictEqual(job2.data, 'job-data-2');
-        return queue.stat();
-      })
-      .then(function (stats) {
-        assert.propertyVal(stats, 'inactiveCount', 2);
-        return queue.add('job-data-2');
-      });
+  it('should add jobs', async function () {
+    var idJob1 = await queue.add('job-data-1');
+    var job1 = await queue.get(idJob1);
+    assert.isDefined(job1.id);
+    assert.strictEqual(job1.state, 'inactive');
+    assert.typeOf(job1.created, 'number');
+    assert.strictEqual(job1.data, 'job-data-1');
+
+    var stats = await queue.stat();
+    assert.propertyVal(stats, 'inactiveCount', 1);
+
+    var idJob2 = await queue.add('job-data-2');
+    var job2 = await queue.get(idJob2);
+    assert.isDefined(job2.id);
+    assert.notEqual(idJob1, job2.id);
+    assert.strictEqual(job2.data, 'job-data-2');
+
+    stats = await queue.stat();
+    assert.propertyVal(stats, 'inactiveCount', 2);
   });
 
-  it('should add a job with priority', function () {
-    return queue.add(1, { priority: 5 })
-      .then(function (id) {
-        return queue.get(id);
-      })
-      .then(function (job) {
-        assert.strictEqual(job.priority, 5);
-      });
+  it('should add a job with priority', async function () {
+    var id = await queue.add(1, { priority: 5 });
+    var job = await queue.get(id);
+    assert.strictEqual(job.priority, 5);
   });
 
-  it('should ignore duplicate job when active', function () {
-    var id1;
-    return queue.config({ unique: true })
-      .then(function () {
-        return queue.add(1);
-      })
-      .then(function (id) {
-        id1 = id;
-        return queue.retrieve();
-      })
-      .then(function (result) {
-        return queue.add(1);
-      })
-      .then(function (id2) {
-        assert.strictEqual(id1, id2);
-      });
+  it('should ignore duplicate job when active', async function () {
+    await queue.config({ unique: true });
+
+    var id1 = await queue.add(1);
+    await queue.retrieve();
+    var id2 = await queue.add(1);
+    assert.strictEqual(id1, id2);
   });
 
-  it('should ignore duplicate job when delayed', function () {
-    var id1;
-    return queue.config({ unique: true })
-      .then(function () {
-        return queue.add(1, {
-          runAt: Date.now() + 100000
-        });
-      })
-      .then(function (id) {
-        id1 = id;
-        return queue.add(1);
-      })
-      .then(function (id2) {
-        assert.strictEqual(id1, id2);
-      });
+  it('should ignore duplicate job when delayed', async function () {
+    await queue.config({ unique: true });
+
+    var id1 = await queue.add(1, { runAt: Date.now() + 100000 });
+    var id2 = await queue.add(1);
+    assert.strictEqual(id1, id2);
   });
 
-  it('should allow duplicate job when failed', function () {
-    var id1;
-    return queue.config({ unique: true })
-      .then(function () {
-        return queue.add(1);
-      })
-      .then(function (id) {
-        id1 = id;
-      })
-      .then(function () {
-        return queue.retrieve();
-      })
-      .then(function (result) {
-        return queue.acknowledge(result.id, new Error('failed'));
-      })
-      .then(function () {
-        return queue.add(1);
-      })
-      .then(function (id2) {
-        assert.notStrictEqual(id1, id2);
-      });
+  it('should allow duplicate job when failed', async function () {
+    await queue.config({ unique: true });
+
+    var id1 = await queue.add(1);
+    var retrieval = await queue.retrieve();
+    await queue.acknowledge(retrieval.id, new Error('failed'));
+    var id2 = await queue.add(1);
+    assert.notStrictEqual(id1, id2);
   });
 
-  it('should allow duplicate job when completed', function () {
-    var id1;
-    return queue.config({ unique: true })
-      .then(function () {
-        return queue.add(1);
-      })
-      .then(function (id) {
-        id1 = id;
-      })
-      .then(function () {
-        return queue.retrieve();
-      })
-      .then(function (result) {
-        return queue.acknowledge(result.id);
-      })
-      .then(function () {
-        return queue.add(1);
-      })
-      .then(function (id2) {
-        assert.notStrictEqual(id1, id2);
-      });
+  it('should allow duplicate job when completed', async function () {
+    await queue.config({ unique: true });
+
+    var id1 = await queue.add(1);
+    var retrieval = await queue.retrieve();
+    await queue.acknowledge(retrieval.id);
+    var id2 = await queue.add(1);
+    assert.notStrictEqual(id1, id2);
   });
 
 });
