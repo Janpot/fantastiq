30a2488f2ea2cc3f442057582a5d3c4369ee4f2d
Jan Potoms <potoms.jan@gmail.com>
2015-06-28 15:55:01 +0200

    Convert some more tests to async/await

--- test/Queue._runCleanupCycle.spec.js
+++ test/Queue._runCleanupCycle.spec.js
@@ -3,6 +3,7 @@
 var redis = require('then-redis');
 var Queue = require('../lib/Queue');
 var assert = require('chai').assert;
+var Promise = require('bluebird');
 var sinon = require('sinon');
 
 describe('Queue._runCleanupCycle', function () {
@@ -24,87 +25,73 @@ describe('Queue._runCleanupCycle', function () {
     }
   });
 
-  it('should clean up jobs when expired', function () {
+  it('should clean up jobs when expired', async function () {
     clock = sinon.useFakeTimers(Date.now());
-    return queue.addN([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-      .map(function () {
-        return queue.retrieve().get('id').bind(queue).then(queue.get);
-      })
-      .map(function (job) {
-        var error = ((job.data % 2) === 0) ? new Error('Failed') : null;
-        return queue.acknowledge(job.id, error);
-      })
-      .then(function () {
-        return queue.config({
-          removeFailedAfter: 5000, removeCompletedAfter: 10000
-        });
-      })
-      .then(function () {
-        clock.tick(4999);
-        return queue._runCleanupCycle();
-      })
-      .then(function (cleaned) {
-        assert.strictEqual(cleaned, 0);
-        return queue.stat();
-      })
-      .then(function (stats) {
-        assert.propertyVal(stats, 'completedCount', 5);
-        assert.propertyVal(stats, 'failedCount', 5);
-        clock.tick(2);
-        return queue._runCleanupCycle();
-      })
-      .then(function (cleaned) {
-        assert.strictEqual(cleaned, 5);
-        return queue.stat();
-      })
-      .then(function (stats) {
-        assert.propertyVal(stats, 'completedCount', 5);
-        assert.propertyVal(stats, 'failedCount', 0);
-        clock.tick(4998);
-        return queue.stat();
-      })
-      .then(function (stats) {
-        assert.propertyVal(stats, 'completedCount', 5);
-        assert.propertyVal(stats, 'failedCount', 0);
-        clock.tick(2);
-        return queue._runCleanupCycle();
-      })
-      .then(function (cleaned) {
-        assert.strictEqual(cleaned, 5);
-        return queue.stat();
-      })
-      .then(function (stats) {
-        assert.propertyVal(stats, 'completedCount', 0);
-        assert.propertyVal(stats, 'failedCount', 0);
-      });
+    var ids = await queue.addN([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    await Promise.each(ids, async function () {
+      var result = await queue.retrieve();
+      var error = ((result.data % 2) === 0) ? new Error('Failed') : null;
+      queue.acknowledge(result.id, error);
+    });
+
+    await queue.config({
+      removeFailedAfter: 5000, removeCompletedAfter: 10000
+    });
+
+    clock.tick(4999);
+    var cleanedCount = await queue._runCleanupCycle();
+    assert.strictEqual(cleanedCount, 0);
+
+    var stats = await queue.stat();
+    assert.propertyVal(stats, 'completedCount', 5);
+    assert.propertyVal(stats, 'failedCount', 5);
+
+    clock.tick(2);
+    cleanedCount = await queue._runCleanupCycle();
+    assert.strictEqual(cleanedCount, 5);
+
+    stats = await queue.stat();
+    assert.propertyVal(stats, 'completedCount', 5);
+    assert.propertyVal(stats, 'failedCount', 0);
+
+    clock.tick(4998);
+    stats = await queue.stat();
+    assert.propertyVal(stats, 'completedCount', 5);
+    assert.propertyVal(stats, 'failedCount', 0);
+
+    clock.tick(2);
+    cleanedCount = await queue._runCleanupCycle();
+    assert.strictEqual(cleanedCount, 5);
+
+    stats = await queue.stat();
+    assert.propertyVal(stats, 'completedCount', 0);
+    assert.propertyVal(stats, 'failedCount', 0);
   });
 
-  it('should return 0 when locked', function () {
+  it('should return 0 when locked', async function () {
     clock = sinon.useFakeTimers(Date.now());
-    return queue.addN([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-      .map(function () {
-        return queue.retrieve().get('id').bind(queue).then(queue.get);
-      })
-      .map(function (job) {
-        var error = ((job.data % 2) === 0) ? new Error('Failed') : null;
-        return queue.acknowledge(job.id, error);
-      })
-      .then(function () {
-        return queue.config({
-          removeFailedAfter: 1000, removeCompletedAfter: 2000
-        });
-      })
-      .then(function () {
-        clock.tick(1500);
-        return queue._runCleanupCycle(10000);
-      })
-      .then(function (cleaned) {
-        assert.strictEqual(cleaned, 5);
-        return queue._runCleanupCycle(10000);
-      })
-      .then(function (cleaned) {
-        assert.strictEqual(cleaned, 0);
-      });
+
+    var ids = await queue.addN([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    await Promise.each(ids, async function () {
+      var result = await queue.retrieve();
+      var error = ((result.data % 2) === 0) ? new Error('Failed') : null;
+      queue.acknowledge(result.id, error);
+    });
+
+    await queue.config({
+      removeFailedAfter: 1000, removeCompletedAfter: 2000
+    });
+
+    clock.tick(1500);
+    var cleanedCount = await queue._runCleanupCycle(10000);
+    assert.strictEqual(cleanedCount, 5);
+
+    clock.tick(1000);
+    // completed ones are now ready to be cleaned but the lock should prevent it
+    cleanedCount = await queue._runCleanupCycle(10000);
+    assert.strictEqual(cleanedCount, 0);
   });
 
 });
--- test/Queue._runDelayedCycle.spec.js
+++ test/Queue._runDelayedCycle.spec.js
@@ -24,72 +24,50 @@ describe('Queue._runDelayedCycle', function () {
     }
   });
 
-  it('should move delayed jobs when delay expires', function () {
+  it('should move delayed jobs when delay expires', async function () {
     var now = Date.now();
     clock = sinon.useFakeTimers(now);
-    var id = null;
-    return queue.add(1, { runAt: now + 1000 })
-      .then(function (_id) {
-        id = _id;
-        clock.tick(999);
-        return queue._runDelayedCycle();
-      })
-      .then(function (count) {
-        assert.strictEqual(count, 0);
-        return queue.get(id);
-      })
-      .then(function (job) {
-        assert.propertyVal(job, 'state', 'delayed');
-        return queue.stat();
-      })
-      .then(function (stats) {
-        assert.propertyVal(stats, 'totalCount', 1);
-        assert.propertyVal(stats, 'delayedCount', 1);
-        return queue.retrieve();
-      })
-      .then(function (result) {
-        assert.isNull(result.id);
-        clock.tick(2);
-        return queue._runDelayedCycle();
-      })
-      .then(function (count) {
-        assert.strictEqual(count, 1);
-        return queue.get(id);
-      })
-      .then(function (job) {
-        assert.propertyVal(job, 'state', 'inactive');
-        return queue.retrieve();
-      })
-      .then(function (result) {
-        assert.propertyVal(result, 'id', id);
-      });
+    var id = await queue.add(1, { runAt: now + 1000 });
+
+    clock.tick(999);
+    var activatedCount = await queue._runDelayedCycle();
+    assert.strictEqual(activatedCount, 0);
+
+    var job = await queue.get(id);
+    assert.propertyVal(job, 'state', 'delayed');
+
+    var stats = await queue.stat();
+    assert.propertyVal(stats, 'totalCount', 1);
+    assert.propertyVal(stats, 'delayedCount', 1);
+
+    var retrieval = await queue.retrieve();
+    assert.isNull(retrieval.id);
+
+    clock.tick(2);
+    activatedCount = await queue._runDelayedCycle();
+    assert.strictEqual(activatedCount, 1);
+
+    job = await queue.get(id);
+    assert.propertyVal(job, 'state', 'inactive');
+
+    retrieval = await queue.retrieve();
+    assert.propertyVal(retrieval, 'id', id);
   });
 
-  it('should preserve priority', function () {
+  it('should preserve priority', async function () {
     var now = Date.now();
     clock = sinon.useFakeTimers(now);
-    var id1 = null;
-    var id2 = null;
-    return queue.add(1, { priority: 10, runAt: now + 1000 })
-      .then(function (_id1) {
-        id1 = _id1;
-        return queue.add(1, { priority: 0, runAt: now + 1000 });
-      })
-      .then(function (_id2) {
-        id2 = _id2;
-        clock.tick(1001);
-        return queue._runDelayedCycle();
-      })
-      .then(function () {
-        return queue.retrieve();
-      })
-      .then(function (job) {
-        assert.propertyVal(job, 'id', id2);
-        return queue.retrieve();
-      })
-      .then(function (job) {
-        assert.propertyVal(job, 'id', id1);
-      });
+    var id1 = await queue.add(1, { priority: 10, runAt: now + 1000 });
+    var id2 = await queue.add(1, { priority: 0, runAt: now + 1000 });
+
+    clock.tick(1001);
+    await queue._runDelayedCycle();
+
+    var retrieval = await queue.retrieve();
+    assert.propertyVal(retrieval, 'id', id2);
+
+    retrieval = await queue.retrieve();
+    assert.propertyVal(retrieval, 'id', id1);
   });
 
 });
