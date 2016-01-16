488782abd23142f08aa44dbe9fb7beb4d9b3810f
Jan Potoms <potoms.jan@gmail.com>
2016-01-16 16:42:21 +0100

    Add worker for maintenance cycles

---
+++ test/IntervalWorker.spec.js
@@ -0,0 +1,164 @@
+'use strict';
+
+var IntervalWorker = require('../lib/IntervalWorker');
+var Promise = require('bluebird');
+var assert = require('chai').assert;
+var sinon = require('sinon');
+
+describe('IntervalWorker', function () {
+  var delayStub = null;
+  var t = 0;
+  
+  function stubDelay() {
+    var result = {
+      total: 0
+    };
+    delayStub = sinon.stub(global, 'setTimeout', function (fn, time) {
+      result.total += time;
+      setImmediate(fn);
+    });
+    return result;
+  }
+  
+  afterEach(function () {
+    if (delayStub) {
+      delayStub.restore();
+      delayStub = null;
+    }
+  })
+  
+  it('should work synchronously', function () {
+    var i = 0;
+    var worker = new IntervalWorker(function () {
+      i += 1;
+      if (i >= 3) {
+        worker.stop();
+        return;
+      }
+      return i;
+    }, 0);
+    return worker.start()
+      .then(function () {
+        assert.strictEqual(i, 3);
+      })
+  });
+  
+  it('should work asynchronously', function () {
+    var i = 0;
+    var worker = new IntervalWorker(function () {
+      i += 1;
+      if (i >= 3) {
+        worker.stop();
+        return Promise.delay(10);
+      }
+      return Promise.delay(10).return(i);
+    }, 0);
+    return worker.start()
+      .then(function () {
+        assert.strictEqual(i, 3);
+      })
+  });
+  
+  it('should stop when throwing', function () {
+    var i = 0;
+    var expectedErr = new Error('Some error');
+    var worker = new IntervalWorker(function () {
+      i += 1;
+      if (i >= 3) {
+        throw expectedErr;
+      }
+      return;
+    }, 0);
+    return worker.start()
+      .then(assert.fail, function (err) {
+        assert.strictEqual(i, 3);
+        assert.strictEqual(err, expectedErr);
+      })
+  });
+  
+  it('should stop when rejected', function () {
+    var i = 0;
+    var expectedErr = new Error('Some error');
+    var worker = new IntervalWorker(function () {
+      i += 1;
+      if (i >= 3) {
+        return Promise.delay(10).throw(expectedErr);
+      }
+      return;
+    }, 0);
+    return worker.start()
+      .then(assert.fail, function (err) {
+        assert.strictEqual(i, 3);
+        assert.strictEqual(err, expectedErr);
+      })
+  });
+  
+  it('should restart', function () {
+    var i = 0;
+    var goal = 3;
+    var worker = new IntervalWorker(function () {
+      i += 1;
+      if (i >= goal) {
+        worker.stop();
+      }
+      return i;
+    }, 0);
+    return worker.start()
+      .then(function () {
+        assert.strictEqual(i, 3);
+        goal = 7;
+        return worker.start();
+      })
+      .then(function () {
+        assert.strictEqual(i, 7);
+      });
+  });
+  
+  it('should delay between runs', function () {
+    var delayStub = stubDelay();
+    
+    var i = 0;
+    var times = [];
+    var worker = new IntervalWorker(function () {
+      times.push(delayStub.total);
+      i += 1;
+      if (i === 3) {
+        worker.setDelay(300);
+      } else if (i >= 5) {
+        worker.stop();
+      }
+      return i;
+    }, 101);
+    return worker.start()
+      .then(function () {
+        assert.strictEqual(i, 5);
+        assert.deepEqual(times, [ 0, 101, 202, 502, 802 ])
+      });
+  });
+
+  it('should cancel workfunction', function () {
+    var isCanceled = false;
+    var worker = new IntervalWorker(function () {
+      return new Promise(function(resolve, reject, onCancel) {
+        onCancel(function () {
+          isCanceled = true;
+        });
+      });
+    });
+    worker.start()
+    return Promise.delay(10)
+      .then(function () {
+        return worker.stop()
+      })
+      .then(function () {
+        assert.isTrue(isCanceled);
+      });
+  });
+
+  it('shouldn\t error when stopped idle', function () {
+    var worker = new IntervalWorker(function () {
+      return 0
+    });
+    return worker.stop();
+  });
+});
--- test/_setup.spec.js
+++ test/_setup.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Promise = require('bluebird');
+Promise.config({ cancellation: true });
 
 before(function () {
   Error.stackTraceLimit = Infinity;
